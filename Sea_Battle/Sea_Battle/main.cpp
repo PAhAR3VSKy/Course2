6e7e115a1f5961518b64a3341ab721be466ab8a4
paharevsky <jo.2015@yandex.ru>
2020-06-11 21:56:15 +0700

    edit map

--- Sea_Battle/Sea_Battle/main.cpp
+++ Sea_Battle/Sea_Battle/main.cpp
@@ -1,4 +1,6 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include "map.h"
 
 using namespace sf; // подключаем пространство имён sf
 
@@ -18,7 +20,9 @@ Battle::Battle(String F)
 {
 	file = F;
 	image.loadFromFile("..\\images\\" + file);
-
+	texture.loadFromImage(image);
+	sprite.setTexture(texture);
+	sprite.setTextureRect(IntRect(50, 50, 50, 50));
 }
 
 int main()
@@ -36,6 +40,8 @@ int main()
 	map_sprite.setTexture(map_texture);
 	map_sprite.setPosition(0, 0);
 
+	Battle ship("ship.png");
+
 	// Главный цикл приложения: выполняется, пока открыто окно
 	while (window.isOpen())
 	{
@@ -50,10 +56,30 @@ int main()
 		// Установка цвета фона
 		window.clear();
 
-		
 
 		// Отрисовка круга
 		window.draw(map_sprite);
+		setRandShip(4, 1);
+		setRandShip(3, 2);
+		setRandShip(2, 3);
+		setRandShip(1, 4);
+		for (int i = 0; i < HEIGHT_MAP; i++)
+		{
+			for (int j = 0; j < WIDTH_MAP; j++)
+			{
+				if (TileMap[i][j] == '1')
+				{
+					ship.sprite.setTextureRect(IntRect(50, 50, 50, 50));
+
+					ship.sprite.setPosition(j * 50 + 50, i * 50 + 50);
+					window.draw(ship.sprite);
+				}
+
+			}
+		}
+		/*ship.sprite.setPosition(50, 50);
+
+		window.draw(ship.sprite);*/
 
 		// Отрисовка окна
 		window.display();
