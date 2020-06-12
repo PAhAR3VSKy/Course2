a4105b5e400d09003d9e0d7ba9b0dc9e79b4ecea
paharevsky <jo.2015@yandex.ru>
2020-06-12 23:19:27 +0700

    add server

--- Sea_Battle/Sea_Battle/main.cpp
+++ Sea_Battle/Sea_Battle/main.cpp
@@ -1,6 +1,11 @@
+#pragma comment(lib, "ws2_32.lib")
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "map.h"
+#include "game.h"
+#include "windows.h"
+#include <winsock2.h>
+#pragma warning(disable: 4996)
 
 using namespace sf; // подключаем пространство имЄн sf
 
@@ -14,6 +19,11 @@ public:
 	Texture texture;
 	Sprite sprite;
 	Battle(String);
+	Battle() {};
+	void setX(int);
+	void setY(int);
+	int getX();
+	int getY();
 };
 
 Battle::Battle(String F)
@@ -24,22 +34,66 @@ Battle::Battle(String F)
 	sprite.setTexture(texture);
 }
 
+void Battle::setX(int x) { this->x = x; }
+void Battle::setY(int y) { this->y = y; }
+int Battle::getX() { return this->x; }
+int Battle::getY() { return this->y; }
+
+int clientSend(Battle data)
+{
+
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1); //¬ерси€ библиотеки
+	if (WSAStartup(DLLVersion, &wsaData) != 0) //«агрузка библиотеки
+	{
+		std::cout << "Error" << std::endl;
+		exit(1);
+	};
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(11111);
+	addr.sin_family = AF_INET;
+
+	SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
+	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) { // ѕроверка успешности соединени€
+		std::cout << "Client: failed connect to server \n";
+		return 1;
+	};
+	std::cout << "Connected" << std::endl;
+
+	int sender = 1;
+	int x, y;
+	send(Connection, (char*)&sender, sizeof(sender), NULL);
+
+	recv(Connection, (char*)&x, sizeof(x), NULL);
+	recv(Connection, (char*)&y, sizeof(y), NULL);
+
+	data.setX(x);
+	data.setY(y);
+	return 0;
+}
+
+
 int main()
 {
 	srand(time(NULL));
 	// ќбъект, который, собственно, €вл€етс€ главным окном приложени€
 	RenderWindow window(VideoMode(1200, 600), "SFML Works!");
 
+	Battle data;
+
 	bool isMove = false;
+	bool turn = true;
 	float dX = 0;
 	float dY = 0;
 
-	setRandShipPlayer(4, 1);	//установка кораблей в рандомном месте
+	setRandShipPlayer(4, 1);	//установка кораблей дл€ игрока в рандомном месте
 	setRandShipPlayer(3, 2);
 	setRandShipPlayer(2, 3);
 	setRandShipPlayer(1, 4);
 
-	setRandShipBot(4, 1);	//установка кораблей в рандомном месте
+	setRandShipBot(4, 1);	//установка кораблей дл€ бота в рандомном месте
 	setRandShipBot(3, 2);
 	setRandShipBot(2, 3);
 	setRandShipBot(1, 4);
@@ -85,19 +139,45 @@ int main()
 		}
 		// ”становка цвета фона
 		window.clear();
-
-		if (event.type == Event::MouseButtonPressed)//уничтожение кораблей
-			if (event.key.code == Mouse::Left)
+		switch (turn)
+		{
+		case true:
+			if (event.type == Event::MouseButtonPressed)//уничтожение кораблей
+				if (event.key.code == Mouse::Left)
+				{
+					if (TileMapBot[((int)pos.y / 50) - 1][(((int)pos.x - 600) / 50) - 1] == '1')
+						TileMapBot[((int)pos.y / 50) - 1][(((int)pos.x - 600) / 50) - 1] = 'x';
+					turn = false;
+					Sleep(100);
+				}
+			if (event.type == Event::MouseButtonPressed)//промах
+				if (event.key.code == Mouse::Left)
+				{
+					if (TileMapBot[((int)pos.y / 50) - 1][(((int)pos.x - 600) / 50) - 1] == '0')
+						TileMapBot[((int)pos.y / 50) - 1][(((int)pos.x - 600) / 50) - 1] = '-';
+					turn = false;
+					Sleep(100);
+				}
+			
+			break;
+		case false:
+			int x, y;
+			do
 			{
-				if(TileMapBot[((int)pos.y / 50) - 1][(((int)pos.x-600) / 50) - 1] == '1')
-					TileMapBot[((int)pos.y / 50) - 1][(((int)pos.x-600) / 50)  - 1] = 'x';
-			}
-		if (event.type == Event::MouseButtonPressed)//промах
-			if (event.key.code == Mouse::Left)
-			{
-				if (TileMapBot[((int)pos.y / 50) - 1][(((int)pos.x-600) / 50) - 1] == '0')
-					TileMapBot[((int)pos.y / 50) - 1][(((int)pos.x-600) / 50) - 1] = '-';
-			}
+				clientSend(data);
+				x = data.getX();
+				y = data.getY();
+			} while (TileMap[x][y] == 'x' || TileMap[x][y] == '-');
+			if (TileMap[x][y] == '1')
+				TileMap[x][y] = 'x';
+			if (TileMap[x][y] == '0')
+				TileMap[x][y] = '-';
+			turn = true;
+			break;
+		default:
+			break;
+		}
+		
 		// ќтрисовка карты
 		window.draw(map_sprite);
 		window.draw(map_sprite_bot);
@@ -112,6 +192,21 @@ int main()
 					ship.sprite.setPosition(j * 50 + 50, i * 50 + 50);
 					window.draw(ship.sprite);
 				}
+				if (TileMap[i][j] == 'x')
+				{
+					ship_rip.sprite.setTextureRect(IntRect(50, 50, 50, 50));
+
+					ship_rip.sprite.setPosition(j * 50 + 50, i * 50 + 50);
+					window.draw(ship_rip.sprite);
+				}
+				if (TileMap[i][j] == '-')
+				{
+					ship_miss.sprite.setTextureRect(IntRect(50, 50, 50, 50));
+
+					ship_miss.sprite.setPosition(j * 50 + 63, i * 50 + 63);
+					window.draw(ship_miss.sprite);
+				}
+				////////////////////////////////////////////////////////////////////
 				if (TileMapBot[i][j] == 'x')
 				{
 					ship_rip.sprite.setTextureRect(IntRect(50, 50, 50, 50));
