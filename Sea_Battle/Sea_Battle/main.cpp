5afe1be9cdb5472fec00e35146f94ac854a4a541
paharevsky <jo.2015@yandex.ru>
2020-06-12 19:50:03 +0700

    add 2 map

--- Sea_Battle/Sea_Battle/main.cpp
+++ Sea_Battle/Sea_Battle/main.cpp
@@ -22,22 +22,27 @@ Battle::Battle(String F)
 	image.loadFromFile("..\\images\\" + file);
 	texture.loadFromImage(image);
 	sprite.setTexture(texture);
-	sprite.setTextureRect(IntRect(50, 50, 50, 50));
 }
 
 int main()
 {
+	srand(time(NULL));
 	// ќбъект, который, собственно, €вл€етс€ главным окном приложени€
-	RenderWindow window(VideoMode(800, 600), "SFML Works!");
+	RenderWindow window(VideoMode(1200, 600), "SFML Works!");
 
 	bool isMove = false;
 	float dX = 0;
 	float dY = 0;
 
-	setRandShip(4, 1);	//установка кораблей в рандомном месте
-	setRandShip(3, 2);
-	setRandShip(2, 3);
-	setRandShip(1, 4);
+	setRandShipPlayer(4, 1);	//установка кораблей в рандомном месте
+	setRandShipPlayer(3, 2);
+	setRandShipPlayer(2, 3);
+	setRandShipPlayer(1, 4);
+
+	setRandShipBot(4, 1);	//установка кораблей в рандомном месте
+	setRandShipBot(3, 2);
+	setRandShipBot(2, 3);
+	setRandShipBot(1, 4);
 
 	Image map_images;
 	map_images.loadFromFile("..\\images\\map.png");
@@ -45,19 +50,27 @@ int main()
 	Texture map_texture;
 	map_texture.loadFromImage(map_images);
 
-	Sprite map_sprite;
+	Sprite map_sprite;	//спрайт карты игрока
 	map_sprite.setTexture(map_texture);
 	map_sprite.setPosition(0, 0);
 
+	Sprite map_sprite_bot;	//спрайт карты бота
+	map_sprite_bot.setTexture(map_texture);
+	map_sprite_bot.setPosition(600, 0);
+
 	Battle ship("ship.png");
 	Battle ship_rip("ship_rip.png");
+	Battle ship_miss("miss.png");
+	ship_miss.texture.setSmooth(true);	//сглаживание на спрайте промаха
+
+	ship_miss.sprite.scale(0.5, 0.5);	//уменьшили спрайт промаха
 
 	// √лавный цикл приложени€: выполн€етс€, пока открыто окно
 	while (window.isOpen())
 	{
 
 		Vector2i pixelPos = Mouse::getPosition(window);
-		Vector2f pos = window.mapPixelToCoords(pixelPos);
+		Vector2f pos = window.mapPixelToCoords(pixelPos);	//получение координат мышки
 		
 		// ќбрабатываем очередь событий в цикле
 		Event event;
@@ -73,14 +86,21 @@ int main()
 		// ”становка цвета фона
 		window.clear();
 
-		if (event.type == Event::MouseButtonPressed)//удаление частей кораблей
+		if (event.type == Event::MouseButtonPressed)//уничтожение кораблей
 			if (event.key.code == Mouse::Left)
 			{
-				if(TileMap[((int)pos.y / 50) - 1][((int)pos.x / 50) - 1] == '1')
-					TileMap[((int)pos.y / 50) - 1][((int)pos.x / 50) - 1] = 'x';
+				if(TileMapBot[((int)pos.y / 50) - 1][(((int)pos.x-600) / 50) - 1] == '1')
+					TileMapBot[((int)pos.y / 50) - 1][(((int)pos.x-600) / 50)  - 1] = 'x';
 			}
-		// ќтрисовка кораблей
+		if (event.type == Event::MouseButtonPressed)//промах
+			if (event.key.code == Mouse::Left)
+			{
+				if (TileMapBot[((int)pos.y / 50) - 1][(((int)pos.x-600) / 50) - 1] == '0')
+					TileMapBot[((int)pos.y / 50) - 1][(((int)pos.x-600) / 50) - 1] = '-';
+			}
+		// ќтрисовка карты
 		window.draw(map_sprite);
+		window.draw(map_sprite_bot);
 		for (int i = 0; i < HEIGHT_MAP; i++)
 		{
 			for (int j = 0; j < WIDTH_MAP; j++)
@@ -92,20 +112,24 @@ int main()
 					ship.sprite.setPosition(j * 50 + 50, i * 50 + 50);
 					window.draw(ship.sprite);
 				}
-				if (TileMap[i][j] == 'x')
+				if (TileMapBot[i][j] == 'x')
 				{
 					ship_rip.sprite.setTextureRect(IntRect(50, 50, 50, 50));
 
-					ship_rip.sprite.setPosition(j * 50 + 50, i * 50 + 50);
+					ship_rip.sprite.setPosition(j * 50 + 650, i * 50 + 50);
 					window.draw(ship_rip.sprite);
+				}
+				if (TileMapBot[i][j] == '-')
+				{
+					ship_miss.sprite.setTextureRect(IntRect(50, 50, 50, 50));
+
+					ship_miss.sprite.setPosition(j * 50 + 663, i * 50 + 63);
+					window.draw(ship_miss.sprite);
 				}
 			}
 		}
 
-		if (Mouse::isButtonPressed(sf::Mouse::Left))
-		{
-
-		}
+		
 
 		// ќтрисовка окна
 		window.display();
--- Sea_Battle/Sea_Battle/map.h
+++ Sea_Battle/Sea_Battle/map.h
@@ -6,7 +6,7 @@ using namespace sf;
 const int HEIGHT_MAP = 10;
 const int WIDTH_MAP = 10;
 
-String TileMap[HEIGHT_MAP][WIDTH_MAP] = {
+String TileMap[HEIGHT_MAP] = {
 	"0000000000",
 	"0000000000",
 	"0000000000",
@@ -19,19 +19,29 @@ String TileMap[HEIGHT_MAP][WIDTH_MAP] = {
 	"0000000000"
 };
 
-void setRandShip(int sizeShip, int numShips)
+String TileMapBot[HEIGHT_MAP] = {
+	"0000000000",
+	"0000000000",
+	"0000000000",
+	"0000000000",
+	"0000000000",
+	"0000000000",
+	"0000000000",
+	"0000000000",
+	"0000000000",
+	"0000000000"
+};
+
+void setRandShipBot(int sizeShip, int numShips)
 {
-	srand(time(NULL));
+	//srand(time(NULL));
 	int x, y;
 	int dir = 0;
 	int countShip = 0;
 	int countTact = 0;
 	while (countShip < numShips)
 	{
-		/*countTact++;
 
-		if (countTact > 10)
-			break;*/
 
 		x = rand() % 10;
 		y = rand() % 10;
@@ -51,15 +61,105 @@ void setRandShip(int sizeShip, int numShips)
 				break;
 			}
 
-			if (TileMap[x	][y		] == '1' ||
-				TileMap[x	][y+1	] == '1' ||
-				TileMap[x	][y-1	] == '1' ||
-				TileMap[x + 1][y    ] == '1' ||
-				TileMap[x + 1][y + 1] == '1' ||
-				TileMap[x + 1][y - 1] == '1' ||
-				TileMap[x - 1][y    ] == '1' ||
-				TileMap[x - 1][y + 1] == '1' ||
-				TileMap[x - 1][y - 1] == '1')
+			if (TileMapBot[x].getData()[y] == '1' ||
+				TileMapBot[x].getData()[y + 1] == '1' ||
+				TileMapBot[x].getData()[y - 1] == '1' ||
+				TileMapBot[x + 1].getData()[y] == '1' ||
+				TileMapBot[x + 1].getData()[y + 1] == '1' ||
+				TileMapBot[x + 1].getData()[y - 1] == '1' ||
+				TileMapBot[x - 1].getData()[y] == '1' ||
+				TileMapBot[x - 1].getData()[y + 1] == '1' ||
+				TileMapBot[x - 1].getData()[y - 1] == '1')
+			{
+				settings_is_possible = 0;
+				break;
+			}
+
+			switch (dir)
+			{
+			case 0:
+				x++;
+				break;
+			case 1:
+				y++;
+				break;
+			case 2:
+				x--;
+				break;
+			case 3:
+				y--;
+				break;
+			}
+		}
+
+		if (settings_is_possible == 1)
+		{
+			x = temp_x;
+			y = temp_y;
+			for (int i = 0; i < sizeShip; i++)
+			{
+				TileMapBot[x][y] = '1';
+				switch (dir)
+				{
+				case 0:
+					x++;
+					break;
+				case 1:
+					y++;
+					break;
+				case 2:
+					x--;
+					break;
+				case 3:
+					y--;
+					break;
+				default:
+					break;
+				}
+			}
+			countShip++;
+		}
+	}
+}
+
+void setRandShipPlayer(int sizeShip, int numShips)
+{
+	/*srand(time(NULL));*/
+	int x, y;
+	int dir = 0;
+	int countShip = 0;
+	int countTact = 0;
+	while (countShip < numShips)
+	{
+		
+
+		x = rand() % 10;
+		y = rand() % 10;
+
+		int temp_x = x;
+		int temp_y = y;
+
+		dir = rand() % 4;
+
+		bool settings_is_possible = 1;
+
+		for (int i = 0; i < sizeShip; i++)
+		{
+			if (x < 0 || y < 0 || x >= 10 || y >= 10)
+			{
+				settings_is_possible = 0;
+				break;
+			}
+
+			if (TileMap[x	].getData()[y] == '1' ||
+				TileMap[x	].getData()[y+1	] == '1' ||
+				TileMap[x	].getData()[y-1	] == '1' ||
+				TileMap[x + 1].getData()[y    ] == '1' ||
+				TileMap[x + 1].getData()[y + 1] == '1' ||
+				TileMap[x + 1].getData()[y - 1] == '1' ||
+				TileMap[x - 1].getData()[y    ] == '1' ||
+				TileMap[x - 1].getData()[y + 1] == '1' ||
+				TileMap[x - 1].getData()[y - 1] == '1')
 			{
 				settings_is_possible = 0;
 				break;
